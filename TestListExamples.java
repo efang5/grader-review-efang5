9c204fed5cbcd9ec847de48f32ed9507603cb2a8
efang5 <156245927+efang5@users.noreply.github.com>
2024-03-06 12:45:17 -0800

    Update TestListExamples.java

--- TestListExamples.java
+++ TestListExamples.java
@@ -18,4 +18,16 @@ public class TestListExamples {
     List<String> expected = Arrays.asList("a", "a", "b", "c", "d");
     assertEquals(expected, merged);
   }
+
+  @Test(timeout = 500)
+  public void testFilter() {
+    List<String> s1 = Arrays.asList("moon", "b", "moon");
+    List<String> s2 = Arrays.asList("moon", "b");
+    List<String> result1 = ListExamples.filter(s1, new IsMoon());
+    List<String> result2 = ListExamples.filter(s2, new IsMoon()); 
+
+    assertEquals(2, result1.size());
+    assertEquals(1, result2.size());
+    assertEquals(false, result1 == result2);
+  }
 }
